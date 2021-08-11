4fc53cd2f608572de1ca3c12902edc7747034566
deborahadebiyi <16781923+deborahadebiyi@users.noreply.github.com>
2021-08-11 12:31:14 +0100

    feat(PKB-1):initialise express boilerplate code

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const port = 3001;
+const port = 3000;
 const app = express();
 
 dotenv.config();
 
+//middleware
+//body parser to parse requests
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+//routes
+app.get("/", (req, res) => {
+    res.send("Let's get retrospective!");
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
